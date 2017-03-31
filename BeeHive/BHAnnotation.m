69a0ad214fbc20bde64e3a22d0ad0ecae2816593
zhongwuzw <zhongwuzw@qq.com>
2017-03-31 10:50:31 +0800

    fix mach_header warning && remove dynamic system library

--- BeeHive/BHAnnotation.m
+++ BeeHive/BHAnnotation.m
@@ -17,19 +17,6 @@
 #import <objc/message.h>
 #include <mach-o/ldsyms.h>
 
-
-
-
-#ifndef __LP64__
-    #define mach_header mach_header
-    #define section     section
-    #define getsectbynamefromheader getsectbynamefromheader
-#else
-    #define mach_header mach_header_64
-    #define section     section_64
-    #define getsectbynamefromheader getsectbynamefromheader_64
-#endif
-
 NSArray<NSString *>* BHReadConfiguration(char *sectionName,const struct mach_header *mhp);
 static void dyld_callback(const struct mach_header *mhp, intptr_t vmaddr_slide)
 {
@@ -44,8 +31,6 @@ static void dyld_callback(const struct mach_header *mhp, intptr_t vmaddr_slide)
             }
         }
     }
-    
-    
     
     //register services
     NSArray<NSString *> *services = BHReadConfiguration(BeehiveServiceSectName,mhp);
@@ -67,7 +52,6 @@ static void dyld_callback(const struct mach_header *mhp, intptr_t vmaddr_slide)
         }
     }
     
-    
 }
 __attribute__((constructor))
 void initProphet() {
@@ -76,10 +60,15 @@ void initProphet() {
 
 NSArray<NSString *>* BHReadConfiguration(char *sectionName,const struct mach_header *mhp)
 {
-    
     NSMutableArray *configs = [NSMutableArray array];
     unsigned long size = 0;
+#ifndef __LP64__
     uintptr_t *memory = (uintptr_t*)getsectiondata(mhp, SEG_DATA, sectionName, &size);
+#else
+    const struct mach_header_64 *mhp64 = (const struct mach_header_64 *)mhp;
+    uintptr_t *memory = (uintptr_t*)getsectiondata(mhp64, SEG_DATA, sectionName, &size);
+#endif
+    
     unsigned long counter = size/sizeof(void*);
     for(int idx = 0; idx < counter; ++idx){
         char *string = (char*)memory[idx];
