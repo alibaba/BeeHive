7082314aad0840f4bb446ddde586dcdd6581c825
ZoMinster <zominster@gmail.com>
2017-09-05 13:51:52 +0800

    恢复class BeeHive;

--- BeeHive/BHAnnotation.h
+++ BeeHive/BHAnnotation.h
@@ -28,10 +28,10 @@
 
 
 #define BeeHiveMod(name) \
-char * k##name##_mod BeeHiveDATA(BeehiveMods) = ""#name"";
+class BeeHive; char * k##name##_mod BeeHiveDATA(BeehiveMods) = ""#name"";
 
 #define BeeHiveService(servicename,impl) \
-char * k##servicename##_service BeeHiveDATA(BeehiveServices) = "{ \""#servicename"\" : \""#impl"\"}";
+class BeeHive; char * k##servicename##_service BeeHiveDATA(BeehiveServices) = "{ \""#servicename"\" : \""#impl"\"}";
 
 @interface BHAnnotation : NSObject
 
